56a5b062264aab2959968cc6418d4a8b5e34f3a6
Tyler <27779354+printsmith@users.noreply.github.com>
2024-01-10 15:08:58 -0500

    update matrix.c to fix cols not working
    increased int size which fixed issues of col 16 and 18 not registering.

--- keyboards/sp110duo/matrix.c
+++ keyboards/sp110duo/matrix.c
@@ -31,59 +31,60 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // How long the scanning code waits for changed io to settle.
 #define MATRIX_IO_DELAY 30
 
-#define COL_SHIFTER ((uint32_t)1)
-#define ROW_SHIFTER ((uint32_t)1)
+#define COL_SHIFTER ((uint64_t)1)
+#define ROW_SHIFTER ((uint64_t)1)
 
 static const pin_t row_pins[] = MATRIX_ROW_PINS;
 static const pin_t col_pins[] = MATRIX_COL_PINS;
 static matrix_row_t previous_matrix[MATRIX_ROWS];
 
 
-static void select_row(uint16_t row) {
+static void select_row(uint32_t row) {
     setPinOutput(row_pins[row]);
     writePinLow(row_pins[row]);
 }
 
-static void unselect_row(uint16_t row) { 
+static void unselect_row(uint32_t row) { 
     setPinInputHigh(row_pins[row]); 
 }
 
 static void unselect_rows(void) {
-    for (uint16_t x = 0; x < MATRIX_ROWS; x++) {
+    for (uint32_t x = 0; x < MATRIX_ROWS; x++) {
         setPinInputHigh(row_pins[x]);
     }
 }
 
-static void select_col(uint16_t col) {
+static void select_col(uint32_t col) {
     setPinOutput(col_pins[col]);
     writePinLow(col_pins[col]);
 }
 
-static void unselect_col(uint16_t col) {
+static void unselect_col(uint32_t col) {
     setPinInputHigh(col_pins[col]);
 }
 
 static void unselect_cols(void) {
-    for (uint16_t x = 0; x < MATRIX_COLS/2; x++) {
+    for (uint32_t x = 0; x < MATRIX_COLS/2; x++) {
         setPinInputHigh(col_pins[x*2]);
     }
 }
 
 
-static void read_cols_on_row(matrix_row_t current_matrix[], uint16_t current_row) {
+static void read_cols_on_row(matrix_row_t current_matrix[], uint32_t current_row) {
     // Select row and wait for row selection to stabilize
     select_row(current_row);
     wait_us(MATRIX_IO_DELAY);
 
     // For each col...
-    for (uint16_t col_index = 0; col_index < MATRIX_COLS / 2; col_index++) {
+    for (uint32_t col_index = 0; col_index < MATRIX_COLS / 2; col_index++) {
+        uint32_t column_index_bitmask = ROW_SHIFTER << ((col_index * 2) + 1);
         // Check row pin state
         if (readPin(col_pins[col_index*2])) {
             // Pin HI, clear col bit
-            current_matrix[current_row] &= ~(ROW_SHIFTER << ((col_index*2) + 1));
+            current_matrix[current_row] &= ~column_index_bitmask;
         } else {
             // Pin LO, set col bit
-            current_matrix[current_row] |= (ROW_SHIFTER << ((col_index*2) + 1));
+            current_matrix[current_row] |= column_index_bitmask;
         }
     }
 
@@ -91,14 +92,14 @@ static void read_cols_on_row(matrix_row_t current_matrix[], uint16_t current_row
     unselect_row(current_row);
 }
 
-static void read_rows_on_col(matrix_row_t current_matrix[], uint16_t current_col) {
+static void read_rows_on_col(matrix_row_t current_matrix[], uint32_t current_col) {
     // Select col and wait for col selection to stabilize
     select_col(current_col*2);
     wait_us(MATRIX_IO_DELAY);
 
-    uint16_t column_index_bitmask = COL_SHIFTER << (current_col * 2);
+    uint32_t column_index_bitmask = COL_SHIFTER << (current_col * 2);
     // For each row...
-    for (uint16_t row_index = 0; row_index < MATRIX_ROWS; row_index++) {
+    for (uint32_t row_index = 0; row_index < MATRIX_ROWS; row_index++) {
         // Check row pin state
         if (readPin(row_pins[row_index])) {
             // Pin HI, clear col bit
@@ -123,13 +124,13 @@ void matrix_init_custom(void) {
 }
 
 void store_old_matrix(matrix_row_t current_matrix[]) {
-    for (uint16_t i = 0; i < MATRIX_ROWS; i++) {
+    for (uint32_t i = 0; i < MATRIX_ROWS; i++) {
         previous_matrix[i] = current_matrix[i];
     }
 }
 
 bool has_matrix_changed(matrix_row_t current_matrix[]) {
-    for (uint16_t i = 0; i < MATRIX_ROWS; i++) {
+    for (uint32_t i = 0; i < MATRIX_ROWS; i++) {
         if (previous_matrix[i] != current_matrix[i]) return true;
     }
     return false;
@@ -140,12 +141,12 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
     store_old_matrix(current_matrix);
 
     // Set row, read cols
-    for (uint16_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
+    for (uint32_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
         read_cols_on_row(current_matrix, current_row);
     }
 
     // Set col, read rows
-    for (uint16_t current_col = 0; current_col < MATRIX_COLS / 2; current_col++) {
+    for (uint32_t current_col = 0; current_col < MATRIX_COLS / 2; current_col++) {
         read_rows_on_col(current_matrix, current_col);
     }
     return has_matrix_changed(current_matrix);
