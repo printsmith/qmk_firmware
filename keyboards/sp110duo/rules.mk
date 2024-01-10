e7fdf77a6fb59bf7580e4f069ff4a7706e9c3ec2
Tyler <27779354+printsmith@users.noreply.github.com>
2024-01-10 16:37:20 -0500

    remove console enable from rules.mk

--- keyboards/sp110duo/rules.mk
+++ keyboards/sp110duo/rules.mk
@@ -1,7 +1,6 @@
 # This file intentionally left blank
 VIA_ENABLE 		= yes
 CUSTOM_MATRIX 	= lite
-CONSOLE_ENABLE 	= yes
 SRC 			+= matrix.c
 QUANTUM_PAINTER_ENABLE = yes
 QUANTUM_PAINTER_DRIVERS += st7789_spi
